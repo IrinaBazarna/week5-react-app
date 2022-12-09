159abd1cb9da8bc49b692f45176f1c336f1a7d21
IrinaBazarna <108300701+IrinaBazarna@users.noreply.github.com>
2022-12-09 20:27:14 +0200

    Add files via upload

---
+++ Weather.js
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+import WeatherDate from "./WeatherDate";
+import axios from "axios";
+
+function WeatherInf(props) {
+  const [weather, setWeather] = useState({ ready: false });
+  const [city, setCity] = useState(props.city);
+
+  function weatherDate(response) {
+    setWeather({
+      ready: true,
+      date: new Date(response.data.dt * 1000),
+      city: response.data.name,
+      temperature: response.data.main.temp,
+      wind: response.data.wind.speed,
+      humidity: response.data.main.humidity,
+      iconUrl: response.data.weather[0].icon,
+      description: response.data.weather[0].description,
+    });
+  }
+  function search() {
+    const apiKey = "15b6ba0523386a8a73b38b2440a74dea";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+    axios.get(apiUrl).then(weatherDate);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
+
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
+
+  if (weather.ready) {
+    return (
+      <div>
+        <div className="Weather">
+          <form onSubmit={handleSubmit}>
+            <div className="row">
+              <div className="col-9">
+                <input
+                  type="search"
+                  placeholder="Type a city"
+                  onChange={updateCity}
+                  className="form-control"
+                  autoFocus="on"
+                />
+              </div>
+              <div className="col-3">
+                <input type="submit" value="Search" className="btn" />
+              </div>
+            </div>
+          </form>
+        </div>
+        <WeatherDate data={weather} />
+      </div>
+    );
+  } else {
+    search();
+    return "Loading...";
+  }
+}
+export default WeatherInf;
